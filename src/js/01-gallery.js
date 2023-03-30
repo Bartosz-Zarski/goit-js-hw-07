780b6802c15ddfe948c9f7ff61fa6d5af3c0c87e
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-03-30 22:22:18 +0200

    Task 1 almost finished

---
+++ src/js/01-gallery.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+
+const fullGallery = document.querySelector('.gallery');
+
+// STWORZENIE ELEMENTOW W DOM I PODPIECIE ICH
+let galleryEl = galleryItems
+  .map(function (image) {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join('');
+
+fullGallery.insertAdjacentHTML('beforeend', galleryEl);
+
+// ZABLOKOWANIE LINKU PO KLIKNIĘCIU
+fullGallery.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+const galleryFragments = document.querySelectorAll('.gallery__image');
+
+galleryFragments.forEach(image => {
+  image.onclick = () => {
+    const instance = basicLightbox
+      .create(`<img id="modal-lightbox" width="1280" height="853" src="${image.dataset.source}">`)
+      .show();
+
+    document.body.addEventListener('keypress', e => {
+      if (e.key === 'Escape') {
+        instance.close();
+      }
+    });
+  };
+});
